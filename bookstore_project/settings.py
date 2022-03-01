c004fcfd5da01caf468fc832dea288871501bd15
Faris <fariskarim2000@gmail.com>
2022-02-28 20:02:50 -0500

    ch12

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -133,6 +133,8 @@ STATICFILES_FINDERS = [ # new
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
+MEDIA_URL = '/media/'
+MEDIA_ROOT= os.path.join(BASE_DIR,"media")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
